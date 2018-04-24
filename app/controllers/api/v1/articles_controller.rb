cc82ac4958752abe0be07e43ceb40353f461ca33
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-04-23 20:16:27 -0500

    created API for articles

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,50 @@
 class Api::V1::ArticlesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  before_action :basic_auth
+
+  # acá van todas las acciones
+
+
+
   def index
-    articles = Articles.all
+    articles = Article.all
     render json: articles
   end
+
+
+
+def create
+  article = Article.new(article_params)
+  if article.save
+    render json: article, status: :ok
+  else
+    render json: {errors: article.errors}, status: 422
+  end
+
+end
+
+
+def destroy
+  article = Article.find(params[:id])
+   article.destroy
+   render json: {message: "Article deleted"}, status: 200
+end
+
+
+private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |user, password|
+      # Verificar user y password. Por ejemplo (aunque no lo recomendamos):
+      user == "german" && password == "test1234"
+    end
+  end
+
+  def article_params
+      params.require(:article).permit(:title, :body, :published)
+  end
+
+
+
+
 end
